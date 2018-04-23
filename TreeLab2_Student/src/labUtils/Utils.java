347b377bc0bd15567ea35ce447784e153ae9a25e
4035c6 <4035c6@mango.ece.uprm.edu>
2018-04-23 14:23:56 -0400

    Finish exercise 1

--- TreeLab2_Student/src/labUtils/Utils.java
+++ TreeLab2_Student/src/labUtils/Utils.java
@@ -21,8 +21,22 @@ public class Utils {
 	public static LinkedTree<Integer> buildExampleTreeAsLinkedTree() { 
 		LinkedTree<Integer> t = new LinkedTree<>(); 
 		// ADD CODE AS SPECIFIED IN EXERCISE 2
-		
-		
+		Position<Integer> root = t.addRoot(4);
+		Position<Integer> children1 = t.addChild(root, 9);
+		t.addChild(children1, 7);
+		t.addChild(children1, 10);
+		Position<Integer> children2 = t.addChild(root, 20);
+		Position<Integer> children3 = t.addChild(children2, 15);
+		t.addChild(children3, 12);
+		Position<Integer> children4 = t.addChild(children3, 17);
+		t.addChild(children4, 19);
+		Position<Integer> children5 = t.addChild(children2, 21);
+		Position<Integer> children6 = t.addChild(children5, 40);
+		t.addChild(children6, 30);
+		t.addChild(children6, 45);
+
+
+
 		return t; 
 	}
 	
